73fbf52222c41e629165e3df9e4a7368758d9e02
Taiwan123123123 <89787486+Taiwan123123123@users.noreply.github.com>
2021-09-01 19:22:54 +0800

    Update polandball.php

--- polandball.php
+++ polandball.php
@@ -8,5 +8,12 @@ $email_body = "名字(暱稱):$name.\n".
                  "為什麼會知道波蘭球:$howyouknowpolandball.\n";
 $to = "diegogamingtw@gmail.com";
 
+$headers = "From $email_from \r\n";
+
+$headers .= "Reply to: $name1 \r\n"
+  
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: polandball.html")
 
 ?>
